7300afa8200406d73fa828afd4965e2aeb632a6e
Silas Torres Rocha <43487074+SilasTorresRocha@users.noreply.github.com>
2025-06-13 16:51:10 -0300

    1.0.1.1

--- assistente-backend/planner/views.py
+++ assistente-backend/planner/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def oi(request):
+    return JsonResponse({'message': 'Teste de  planner!'})
+
